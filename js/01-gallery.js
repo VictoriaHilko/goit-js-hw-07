b8614c31a552c6144e28edd91f9b1e1143846cba
VictoriaHilko <47423052+VictoriaHilko@users.noreply.github.com>
2023-07-23 16:46:17 +0300

    add task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,59 @@
+// імпортуємо із ./gallery-items.js дані масиву об'єктів galleryItems
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+
+// створили змінну, де записали посилання на <ul class="gallery"></ul>
+const galleryList = document.querySelector('.gallery');
+
+// markup - змінна для генерування списку li
+const markup = galleryItems
+    .map((picture) =>
+        `<li class="gallery__item">
+    <img class="gallery__image" 
+    src="${picture.preview}" 
+    alt="${picture.description}">
+    </li>`)
+    .join("");
+
+//   додаємо всередину ul дані із змінної markup
+galleryList.insertAdjacentHTML("beforeend", markup);
+
+
+
+
+//додали обробник події на клик по ul
+galleryList.addEventListener('click', handleClick);
+
+//   функція обробник події клик по ul
+
+function handleClick(event) {
+
+    // console.log(event.target);
+
+    // console.log();
+
+
+    // const originalPicture = basicLightbox.create(`
+    // <div>
+    // <img class="gallery__image" 
+    // src="${event.target.src}" 
+    // alt="${event.target.description}">
+    // </div>
+    // `);
+
+    // originalPicture.show();
+
+
+    for (let i = 0; i < galleryItems.length; i += 1){
+        if (event.target.getAttribute('src') === galleryItems[i].preview) {
+            const originalPicture = basicLightbox.create(`
+    <div>
+    <img class="gallery__image" 
+    src="${galleryItems[i].original}" 
+    alt="${event.target.description}">
+    </div>
+    `);
+    originalPicture.show();
+        }
+    }
+}
